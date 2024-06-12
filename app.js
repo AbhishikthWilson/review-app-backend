5a5f886def9d06da91574c558d6acbbd7e096c56
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-12 22:41:37 +0530

    add review api completed

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const reviews= require("./models/review")
+const {reviewModel}= require("./models/review")
 
 const app= express()
 app.use(cors())
+app.use(express.json())
 
-app.post("/",(req,res)=>{
+mongoose.connect("mongodb+srv://abhishikth:achuMon0075@cluster0.38fgaky.mongodb.net/reviewDb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/add",(req,res)=>{
+    let input= req.body
+    let review= new reviewModel(input)
+    review.save()
+    console.log(review)
     res.send("welcome")
 })
 
